9f9219b557322d700fd96fe4c5cd38488278fe3c
anna-follestad-4ss <anna.fehn.follestad@4subsea.com>
2022-10-11 12:09:44 +0000

    Bump version: 0.0.8 → 0.0.9

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -2,7 +2,7 @@ from my_package._module1 import function1, function2
 from my_package._module2 import function3, function4
 from my_package._module3 import Class1 
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 
 __all__ = [
